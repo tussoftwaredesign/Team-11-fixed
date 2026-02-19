a71e594c2f6ff4dd704e2e3e9f43e8f64eb3d6af
funny_money_ <lukeocoonor275a@gmail.com>
2026-02-19 15:40:45 +0000

    Projectiles Basic implementation

---
+++ Assets/EnemyBulletScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    private float timer;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Collision"))
+        {
+            Destroy(gameObject); 
+        }
+        
+    }
+}
